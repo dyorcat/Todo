2cc1877a7bf93e43e318319062d7c719534f873a
dyorcat <yool0417@gmail.com>
2024-01-05 11:15:08 +0900

    서비스구현

--- src/main/kotlin/com/teamsparta/todolist/domain/card/model/Card.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/card/model/Card.kt
@@ -1,24 +1,43 @@
 package com.teamsparta.todolist.domain.card.model
 
+import com.teamsparta.todolist.domain.card.dto.CardResponse
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import java.util.Date
 
 @Entity
 @Table(name = "card")
-class Card(
+class CardEntity(
     @Column(name = "title")
     var title: String,
 
     @Column(name = "content")
-    var content: String? = null
+    var content: String? = null,
+
+    @Column(name = "nickname")
+    var nickname: String,
+
+    @Column(name = "day")
+    var day: Date
 ) {
 
+
+    fun CardEntity.toResponse(): CardResponse {
+        return CardResponse(
+            id = id!!,
+            title = title,
+            content = content,
+            nickname = nickname,
+            day = day
+        )
+    }
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 데이터베이스에 위임해서 id를 자동생성한다. 그래서 nullable
     var id: Long? = null
 
 }
